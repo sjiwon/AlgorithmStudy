613a39c34c4578e33bc12378f75f0a612fb1dee2
SeoJiWon <sjiwon4491@gmail.com>
2022-07-31 18:21:35 +0900

    [TCT] 성적이 낮은 순서로 학생 출력하기 / 정렬 <이름 변경>

--- thisiscodingtest/Week4_Sorting/OrderByGradeASC.java
+++ thisiscodingtest/Week4_Sorting/OrderByGradeASC.java
@@ -8,7 +8,7 @@ import java.util.LinkedList;
 import java.util.Map;
 import java.util.StringTokenizer;
 
-public class OrderOfLowerGrades {
+public class OrderByGradeASC {
 
     static StringTokenizer st;
 
