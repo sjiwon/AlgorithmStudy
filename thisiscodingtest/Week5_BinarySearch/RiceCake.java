efcf648547a3fca0a4c0721ad4a09a7460314587
SeoJiWon <sjiwon4491@gmail.com>
2022-08-02 18:56:41 +0900

    [TCT] 떡볶이 떡 만들기 / BinarySearch

---
+++ thisiscodingtest/Week5_BinarySearch/RiceCake.java
@@ -0,0 +1,56 @@
+package thisiscodingtest.Week5_BinarySearch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class RiceCake {
+
+    static StringTokenizer st;
+    static int maxHeight = 0; // 절단기 설정 최대 높이
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        List<Integer> cake = new ArrayList<>();
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            cake.add(Integer.parseInt(st.nextToken()));
+        }
+
+        cutRiceCake(cake, 0, Collections.max(cake), M);
+        System.out.println(maxHeight);
+    }
+
+    static void cutRiceCake(List<Integer> cake, int left, int right, int M){
+        if(left > right){
+            return;
+        }
+
+        int cut = (left + right) / 2;
+
+        if(getTotalLength(cake, cut) < M){ // 떡 부족
+            cutRiceCake(cake, left, cut - 1, M);
+        } else if(getTotalLength(cake, cut) > M){ // 떡 넘침
+            cutRiceCake(cake, cut + 1, right, M);
+        } else{ // 떡 충족
+            maxHeight = cut;
+        }
+    }
+
+    static int getTotalLength(List<Integer> cake, int H){
+        int result = 0;
+        for (int length : cake) {
+            result += (Math.max(length - H, 0));
+        }
+        return result;
+    }
+}
